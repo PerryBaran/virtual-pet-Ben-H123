3d9e04b2667e6432104e4d58f4e89c958399d40e
Ben-H123 <ben_h321@protonmail.com>
2023-11-08 21:41:37 +0000

    added age method

--- __test__/pet.test.js
+++ __test__/pet.test.js
@@ -1,10 +1,29 @@
 const Pet = require('../src/pet');
 
-
+describe('constructor', () => {
+    it('returns an object', () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
+    });
+});
 
 describe('constructor', () => {
     it('sets the name property', () => {
         const pet = new Pet('Fido');
         expect(pet.name).toEqual('Fido');
+    });
+});
+
+describe('constructor', () => {
+    it('has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('constructor', () => {
+    it('increments the age by 1', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.age).toEqual(1);
     });
 });
